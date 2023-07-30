f6260841b98952bc9ef8be4c5d114f49a0741e1d
kihlh <kihlh@qq.com>
2023-07-30 09:45:13 +0800

    开始支持托盘图标 子键,主键,分割线,选项,事件...

---
+++ source/CPP_Reconfig/util/dll/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <windows.h>
+#include <iostream>
+#include "../../tray.hpp"
+
+using namespace std;
+
+
+
+int main() {
+    auto trays = new hmc_tray::CTrayIcon();
+    trays->Start();
+
+    return 0;
+}
